de4e729d91c26b2f2a5ce1997d0ad035f79e9c78
kana <nicht@s8.xrea.com>
2009-12-28 14:52:26 +0900

    Add notes on events

--- plugins/hello.js
+++ plugins/hello.js
@@ -3,9 +3,11 @@
 register_plugin((function(){
   return {
     ready: function(){
+      // Raised when fnfnen is ready to use.
       alert('i sasara <Esc>g?b');
     },
     new_tweets: function(kw){
+      // Raised when new tweets have been fetched.
       alert(kw.tweets.length + ' tweets!');
     },
   };
