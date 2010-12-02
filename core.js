14cb6e537b1dbe84d6267ca796cb9c887ebc39f2
kana <nicht@s8.xrea.com>
2010-12-03 01:10:28 +0900

    Preference: Revise save() to call set_form()
    For ease of saving and reflecting the current value of a preference from
    script, change Preference.save() to call Preference.set_form().

--- core.js
+++ core.js
@@ -1375,7 +1375,6 @@ function Preference(name, default_value, opt_kw)  //{{{2
 
   this.apply = function (via_external_configuration_p) {
     this.get_form();
-    this.set_form();
     this.save();
     if (via_external_configuration_p) {
       // Leave form content as-is.
@@ -1429,6 +1428,7 @@ function Preference(name, default_value, opt_kw)  //{{{2
 
   this.save = function () {
     $.storage(this.name, this.current_value);
+    this.set_form();
   };
 
   this.set_form = function () {
