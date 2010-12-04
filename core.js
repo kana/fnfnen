8a0f9d1d443b4740bc1d929882aea9bc1c29c113
kana <nicht@s8.xrea.com>
2010-12-04 22:26:25 +0900

    Refactor - Add tokenize_tweet

--- core.js
+++ core.js
@@ -339,17 +339,18 @@ function learn_tweet(tweet_id, right_tweet_p, interactive_p)  //{{{2
   var tweet = tweet_db.get(tweet_id);
   tweet.prafbe_learning_bias = (tweet.prafbe_learning_bias || 0);
 
+  var tokens = tokenize_tweet(tweet);
   var step = function () {
     if (right_tweet_p) {
       if (0 <= tweet.prafbe_learning_bias)
-        prafbe.learn(g_preferences.prafbe_right_dict(), tweet.text);
+        prafbe.learn(g_preferences.prafbe_right_dict(), tokens);
       else
-        prafbe.unlearn(g_preferences.prafbe_wrong_dict(), tweet.text);
+        prafbe.unlearn(g_preferences.prafbe_wrong_dict(), tokens);
     } else {
       if (tweet.prafbe_learning_bias <= 0)
-        prafbe.learn(g_preferences.prafbe_wrong_dict(), tweet.text);
+        prafbe.learn(g_preferences.prafbe_wrong_dict(), tokens);
       else
-        prafbe.unlearn(g_preferences.prafbe_right_dict(), tweet.text);
+        prafbe.unlearn(g_preferences.prafbe_right_dict(), tokens);
     }
     g_prafbe_learning_count++;
     tweet.prafbe_learning_bias += (right_tweet_p ? 1 : -1);
@@ -1313,7 +1314,7 @@ function calculate_spam_probability(tweet)  //{{{2
       || tweet.prafbe_learning_count == null
       || tweet.prafbe_learning_count < g_prafbe_learning_count)
   {
-    var tokens = prafbe.tokenize(tweet.text);
+    var tokens = tokenize_tweet(tweet);
     var itokens = prafbe.list_most_interesting_tokens(
       g_preferences.prafbe_right_dict(),
       g_preferences.prafbe_wrong_dict(),
@@ -1352,6 +1353,14 @@ function save_prafbe_learning_result()  //{{{2
 {
   g_preferences.prafbe_right_dict.save();
   g_preferences.prafbe_wrong_dict.save();
+}
+
+
+
+
+function tokenize_tweet(tweet)  //{{{2
+{
+  return prafbe.tokenize(tweet.text);
 }
 
 
